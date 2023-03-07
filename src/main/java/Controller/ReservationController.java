0b24f9b8fc8929baea66a70c6fab7cb4de27be88
mdiazelt <89318534+mdiazelt@users.noreply.github.com>
2023-03-07 21:17:55 +0000

    first commit

---
+++ src/main/java/Controller/ReservationController.java
@@ -0,0 +1,13 @@
+
+public class ReservationController {
+
+    CreateAccount createAccount;
+
+    public ReservationController() {
+        this.createAccount = new CreateAccount();
+    }
+
+    public Javalin startAPI() {
+        
+    }
+}
