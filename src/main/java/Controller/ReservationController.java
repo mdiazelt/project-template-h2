73a0c1f60e84ab00e8103a2d04faac423dbc08e4
Maria Diaz Eltgroth <mdiazelt@stedwards.edu>
2023-03-08 13:59:31 -0600

    commit

--- src/main/java/Controller/ReservationController.java
+++ src/main/java/Controller/ReservationController.java
@@ -1,13 +1,31 @@
+package Controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Model.Reservation;
+import io.javalin.Javalin;
+//import io.javalin.http.ConnectionSingleton;
 
 public class ReservationController {
-
     CreateAccount createAccount;
 
     public ReservationController() {
+
         this.createAccount = new CreateAccount();
     }
 
     public Javalin startAPI() {
-        
+        Javalin app = Javalin.create();
+
+        //app.post("/register", this::registerNewUser);
+
+        return app;
+    }
+
+    //private void registerNewUser(Contex ctx) {
+        ObjectMapper mapper = new ObjectMapper();
     }
 }
--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,6 +1,15 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
+drop table if exists message;
+drop table if exists account;
+
+CREATE TABLE account (
+    account_id int primary key auto_increment,
+    username varchar(255) unique;
+    password varchar(255)
+);
+
 
 create table reservation (
     reservation_id int primary key auto_increment,
