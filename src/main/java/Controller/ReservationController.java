28b50c55a0bd2fe21dae4cea62061fdd7cb5cd5a
Daiki Anjiki <daikianjiki@gmail.com>
2023-03-08 12:04:04 -0800

    started controller

--- src/main/java/Controller/ReservationController.java
+++ src/main/java/Controller/ReservationController.java
@@ -1,13 +1,25 @@
+package Controller;
+
+import Service.ReservationService;
+
 
 public class ReservationController {
 
     CreateAccount createAccount;
+    ReservationService reservationService;
 
     public ReservationController() {
         this.createAccount = new CreateAccount();
+        this.reservationService = new ReservationService();
     }
 
     public Javalin startAPI() {
-        
+        Javalin app = Javalin.create();
+
+        app.post("/reservations", this::postReservationsHandler);
+        app.get("")
+
+
+        return app;
     }
 }
