acadbc9b26fee1eb5cc41752a42fe95395f51c2c
Maria Diaz Eltgroth <mdiazelt@stedwards.edu>
2023-03-08 15:30:59 -0600

    controller updates

--- src/main/java/Controller/ReservationController.java
+++ src/main/java/Controller/ReservationController.java
@@ -6,26 +6,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import Model.Account;
 import Model.Reservation;
+import Service.AccountService;
 import io.javalin.Javalin;
-//import io.javalin.http.ConnectionSingleton;
+import io.javalin.http.Context;
 
 public class ReservationController {
-    CreateAccount createAccount;
+    AccountService accountService;
 
     public ReservationController() {
 
-        this.createAccount = new CreateAccount();
+        this.accountService = new AccountService();
     }
 
     public Javalin startAPI() {
         Javalin app = Javalin.create();
 
-        //app.post("/register", this::registerNewUser);
+        app.post("/register", this::registerNewUser);
+        app.post("/login", this::userLogin);
+        app.get("accounts/{account_id}/reservations", this::getReservationForAccount);
 
         return app;
     }
 
-    //private void registerNewUser(Contex ctx) {
+
+    private void registerNewUser(Context ctx) {
         ObjectMapper mapper = new ObjectMapper();
+    }
+
+
+    private void userLogin(Context context) {
+    }
+
+    private void getReservationForAccount(Context context) {
     }
 }
--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,21 +1,72 @@
 package DAO;
 
+import java.sql.*;
+
 import Model.Account;
+import Util.ConnectionSingleton;
+
 public class AccountDAO{
 
-    public Account getAccount(String username, String password) {
-        return null;
-    }
-
-    public CharSequence getUsername() {
-        return null;
-    }
-
-    public Object getPassword() {
-        return null;
-    }
-
     public Account createAccount(Account account) {
+        Connection connection = ConnectionSingleton.getConnection();
+        try{
+            String sql = "INSERT INTO account(username, password) VALUES(?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, account.username);
+            preparedStatement.setString(2, account.password);
+
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_account_id =(int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account getAccountById(int account_id){
+        Connection connection = ConnectionSingleton.getConnection();
+        try{
+            String sql = "SELECT * FROM account WHERE account_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, account_id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getInt("account_id"),
+                                                rs.getString("username"),
+                                                rs.getString("password"));
+                return account;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account getAccount(String username, String password){
+        Connection connection = ConnectionSingleton.getConnection();
+        try{
+            String sql = "SELCT * FROM account WHERE username = ? AND password = ?";
+            PreparedStatement preparedStatement = connection .prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getInt("account_id"),
+                                    rs.getString("username"),
+                                    rs.getString("password"));
+                return account;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 }
